5b6a5344bcffc382e10e941d20cbb5625cbfb699
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2019-12-17 15:07:08 -0600

    sql tables exercise

--- students_migration.sql
+++ students_migration.sql
@@ -1,5 +1,10 @@
-Use deimos_db;
-CREATE TABLE students (
-    id INT UNSIGNED NOT NULL AUTO-INCREMENT,
-    first-name
-)
+Use codeup_test_db;
+CREATE TABLE IF NOT EXISTS students (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    birthday DATE,
+    projects TEXT NOT NULL,
+    is_hired TINYINT(4) NOT NULL,
+    PRIMARY KEY(id)
+);
