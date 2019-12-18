d222291ed28800ce4b028ec09afa8e23a3008e91
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2019-12-18 16:31:09 -0600

    real group by

--- group_by_exercsises.sql
+++ group_by_exercsises.sql
@@ -1,2 +1,25 @@
-use titles;
+use employees;
+SELECT DISTINCT title FROM titles;
+
+SELECT DISTINCT first_name, last_name, COUNT(*)
+FROM employees
+WHERE last_name LIKE ('E%')
+AND last_name LIKE ('%e')
+GROUP BY first_name, last_name
+ORDER BY COUNT(*) DESC;
+
+SELECT DISTINCT last_name, COUNT(*)
+FROM employees
+WHERE last_name LIKE ('%q%')
+AND last_name NOT LIKE ('%qu%')
+GROUP BY last_name
+ORDER BY COUNT(*);
+
+SELECT COUNT(first_name)
+FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+GROUP BY gender
+ORDER BY COUNT(first_name) DESC;
 
